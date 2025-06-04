2737f7929844e0226ee9b5762dbc256b490c5c11
Md Azhar Maqsood <mdazharmaqsood@gmail.com>
2025-06-04 22:23:06 +0530

    Time: 0 ms (100%), Space: 19.4 MB (72.71%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,32 +1,46 @@
-class Solution {
-    void dfs(int start, vector<int>&vis, vector<int>adj[]){
-        vis[start]=1;
-        for(int x:adj[start]){
-            if(!vis[x]){
-                dfs(x,vis,adj);
-            }
+class DSU{
+    vector<int> parent, rank;
+    public:
+    DSU(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
         }
     }
+    int find_set(int v){
+        if(v==parent[v])
+            return v;
+        return parent[v]=find_set(parent[v]);
+    }
+    void union_sets(int a, int b){
+        a=find_set(a);
+        b=find_set(b);
+        if(a!=b){
+            if(rank[a]<rank[b])
+                swap(a,b);
+            parent[b]=a;
+            if(rank[a]==rank[b])
+                rank[a]++;
+        }
+    }
+};
+class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<int>adj[n+1];
+        DSU dsu(n);
+        int province = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1 and i!=j){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+                if(isConnected[i][j]==1){
+                    dsu.union_sets(i+1,j+1);
                 }
             }
         }
-        int cnt=0;
-        vector<int>vis(n+1,0);
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                cnt++;
-                dfs(i,vis,adj);
-            }
+        for(int i=1;i<=n;i++){
+            if(dsu.find_set(i)==i)province++;
         }
-        return cnt;
+        return province;
     }
 };
