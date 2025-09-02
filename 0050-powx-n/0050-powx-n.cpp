b92a5d2f674aa3e8f112dfc1e164043ef21b2699
Md Azhar Maqsood <mdazharmaqsood@gmail.com>
2025-09-03 00:09:45 +0530

    Time: 0 ms (100%), Space: 8.7 MB (43.55%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n<0){
+            x=1/x;
+        }
+        long num=labs(n);
+        double ans=1;
+        while(num){
+            if(num&1){
+                ans*=x;
+            }
+            x*=x;
+            num>>=1;
+        }
+        return ans;
+    }
+};
