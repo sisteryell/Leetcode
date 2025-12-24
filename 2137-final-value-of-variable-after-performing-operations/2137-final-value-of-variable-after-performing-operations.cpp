0ec2af3d12d09adebc7c9cca6800de5106367074
Azhar <mdazharmaqsood@gmail.com>
2025-12-24 22:54:22 +0530

    Time: 0 ms (100%), Space: 17.5 MB (74.44%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.cpp
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
-        int ans = 0;
-        for (string x : operations) ans += (x[1] == '+' ? 1 : -1);
+        int ans=0;
+        for(string x:operations){
+            ans+=(x[1]=='+'?1:-1);
+        }
         return ans;
     }
 };
