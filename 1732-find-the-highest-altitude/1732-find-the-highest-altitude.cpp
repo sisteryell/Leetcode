240851965d80517ddfac328304428430cf950237
Md Azhar Maqsood <mdazharmaqsood@gmail.com>
2026-06-20 01:09:31 +0530

    Time: 0 ms (100%), Space: 10.9 MB (44.68%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int sum=0,ans=0;
+        for(int x: gain){
+            sum+=x;
+            ans=max(ans,sum);
+        }
+        return ans;
+    }
+};
