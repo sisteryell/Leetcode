700063a44c6a1fd2e0be11f209ae826df69b2de3
Md Azhar Maqsood <mdazharmaqsood@gmail.com>
2026-08-10 20:13:59 +0530

    Time: 43 ms (57.44%), Space: 16.8 MB (39.1%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    vector<int> dp;
+    bool solve(int n) {
+        if (n == 0) return false;
+        if (dp[n] != -1) return dp[n];
+        for (int i = 1; i * i <= n; i++) {
+            if (!solve(n - i * i)) {
+                return dp[n] = true;
+            }
+        }
+        return dp[n] = false;
+    }
+public:
+    bool winnerSquareGame(int n) {
+        dp.assign(n + 1, -1);
+        return solve(n);
+    }
+};
